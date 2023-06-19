23bdd56a067b3d5507b32bc228691c2133ff07e1
Hwangwoosam <sam1783@naver.com>
2023-06-19 22:40:30 +0900

    DB 관련 기능 추가

--- app/src/main/java/com/example/moneybank/MainActivity.java
+++ app/src/main/java/com/example/moneybank/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.moneybank;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,14 +20,16 @@ import com.example.moneybank.library.CalendarDay;
 import com.example.moneybank.library.DayViewDecorator;
 import com.example.moneybank.library.DayViewFacade;
 import com.example.moneybank.library.MaterialCalendarView;
-import com.example.moneybank.library.format.ArrayWeekDayFormatter;
-import com.example.moneybank.library.format.MonthArrayTitleFormatter;
-import com.example.moneybank.library.format.TitleFormatter;
+import com.example.moneybank.library.OneDayDecorator;
+import com.example.moneybank.library.SaturdayDecorator;
+import com.example.moneybank.library.SundayDecorator;
+import com.example.moneybank.library.format.format.ArrayWeekDayFormatter;
+import com.example.moneybank.library.format.format.MonthArrayTitleFormatter;
+import com.example.moneybank.library.format.format.TitleFormatter;
 
-import java.time.DayOfWeek;
+import java.io.File;
 import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,37 +40,19 @@ public class MainActivity extends AppCompatActivity {
     public TextView paytotal_view ,earntotal_view,sum_total_view , text_view1, monthName_view;
     public Button statistic_Btn,asset_Btn,home_Btn, log_Btn, add_Btn;
 
+    public SQLiteDatabase database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        database = init_database();
         initComponent();
+        initSetting();
+        init_tables();
 
-        calendarView.state()
-                .edit()
-                .setFirstDayOfWeek(DayOfWeek.MONDAY)
-                .commit();
-
-        calendarView.setTitleFormatter(new MonthArrayTitleFormatter(getResources().getTextArray(R.array.custom_months)));
-        calendarView.setWeekDayFormatter(new ArrayWeekDayFormatter(getResources().getTextArray(R.array.custom_weekdays)));
-
-        calendarView.addDecorator(new DayDecorator(this));
-
-        calendarView.setTitleFormatter(new TitleFormatter() {
-            @Override
-            public CharSequence format(CalendarDay day) {
-                LocalDate inputText = day.getDate();
-                Log.d(TAG,"day " + inputText);
-                String[] calendarHeaderElements = inputText.toString().split("-");
-                StringBuilder calendarHeaderBuilder = new StringBuilder();
-                calendarHeaderBuilder.append(calendarHeaderElements[0])
-                        .append(" ")
-                        .append(calendarHeaderElements[1]);
-                Log.d(TAG,"builder: " + calendarHeaderBuilder.toString());
-                return calendarHeaderBuilder.toString();
-            }
-        });
+        database.close();
     }
 
     public void initComponent(){
@@ -96,6 +83,32 @@ public class MainActivity extends AppCompatActivity {
         add_Btn.setVisibility(View.VISIBLE);
     }
 
+    public void initSetting(){
+        calendarView.setTitleFormatter(new MonthArrayTitleFormatter(getResources().getTextArray(R.array.custom_months)));
+        calendarView.setWeekDayFormatter(new ArrayWeekDayFormatter(getResources().getTextArray(R.array.custom_weekdays)));
+
+        calendarView.addDecorators(
+                new DayDecorator(this),
+                new SaturdayDecorator(),
+                new SundayDecorator(),
+                new OneDayDecorator(this)
+        );
+
+        calendarView.setTitleFormatter(new TitleFormatter() {
+            @Override
+            public CharSequence format(CalendarDay day) {
+                LocalDate inputText = day.getDate();
+                Log.d(TAG,"day " + inputText);
+                String[] calendarHeaderElements = inputText.toString().split("-");
+                StringBuilder calendarHeaderBuilder = new StringBuilder();
+                calendarHeaderBuilder.append(calendarHeaderElements[0])
+                        .append(" ")
+                        .append(calendarHeaderElements[1]);
+                Log.d(TAG,"builder: " + calendarHeaderBuilder.toString());
+                return calendarHeaderBuilder.toString();
+            }
+        });
+    }
     private static class DayDecorator implements DayViewDecorator {
         private final Drawable drawable;
 
@@ -111,4 +124,57 @@ public class MainActivity extends AppCompatActivity {
             view.setSelectionDrawable(drawable);
         }
     }
+
+    private SQLiteDatabase init_database(){
+        SQLiteDatabase db = null;
+
+        File file = new File(getFilesDir(),"MoneyBank.db");
+
+        System.out.println("PATH : " + file.toString());
+
+        try{
+            db=SQLiteDatabase.openOrCreateDatabase(file,null);
+        }catch(SQLiteException e){
+            e.printStackTrace();
+        }
+
+        if(db==null){
+            System.out.println(("DB creation failed. " + file.getAbsolutePath()));
+        }
+
+        return db;
+    }
+
+    private void init_tables(){
+
+        if (database != null){
+
+            String CreateAccountTbl = "CREATE TABLE IF NOT EXISTS ACCOUNT_T (" +
+                    "ID "   + "INTEGER " + "PRIMARY KEY " + "AUTOINCREMENT," +
+                    "NAME " + "VARCHAR(255) NOT NULL," +
+                    "TOTAL_SUM " + "INTEGER DEFAULT 0" + ")" ;
+
+            database.execSQL(CreateAccountTbl);
+
+            String CreatePayContent = "CREATE TABLE IF NOT EXISTS PAYCONTENT_T (" +
+                    "ID " + "INTEGER " + "PRIMARY KEY " + "AUTOINCREMENT," +
+                    "PayDATE " + "VARCHAR(255) NOT NULL," +
+                    "MONEYDIREC " + "INTEGER NOT NULL," +
+                    "PRICE " + "INTEGER DEFAULT 0," +
+                    "USAGE " + "VARCHAR(255) NOT NULL" + ")" ;
+
+            database.execSQL(CreatePayContent);
+
+            String CreatePayInfo = "CREATE TABLE IF NOT EXISTS PAYINFO (" +
+                    "ID " + "INTEGER " + "PRIMARY KEY " + "AUTOINCREMENT," +
+                    "ACCOUNTID " + "INTEGER NOT NULL," +
+                    "PAYCONTENTID " + "INTEGER NOT NULL," +
+                    "FOREIGN KEY (ACCOUNTID) " + "REFERENCES ACCOUNT_T(ID)," +
+                    "FOREIGN KEY (PAYCONTENTID) " + "REFERENCES PAYCONTENTID(ID))";
+
+            database.execSQL(CreatePayInfo);
+
+        }
+    }
+
 }
